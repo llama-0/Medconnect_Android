63e511e609bfa93ad5082629a497fa2a1add5918
llama-0 <AnRozan93@mail.ru>
2016-10-02 12:50:51 +0300

    Firebase List Adapter

--- app/src/main/java/com/example/user/medconnect/MainActivity.java
+++ app/src/main/java/com/example/user/medconnect/MainActivity.java
@@ -19,88 +19,43 @@ import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
+import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-  //  private Firebase mRef;
-
-    private EditText mEmailField;
-    private EditText mPasswordField;
-
-    private Button mLoginBtn;
-
-    private FirebaseAuth mAuth;
-
-    private FirebaseAuth.AuthStateListener mAuthListener;
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        mRef = new Firebase("https://medconnect-1c3b5.firebaseio.com/Users");
-        mAuth = FirebaseAuth.getInstance();
+        mListView = (ListView) findViewById(R.id.listView);
 
-        mEmailField = (EditText) findViewById(R.id.emailField);
-        mPasswordField = (EditText) findViewById(R.id.passwordField);
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://medconnect-1c3b5.firebaseio.com/Users");
 
-        mLoginBtn = (Button) findViewById(R.id.loginBtn);
-
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
+        FirebaseListAdapter<String> firebaseListAdapter = new FirebaseListAdapter<String>(
+                this,
+                String.class,
+                android.R.layout.simple_list_item_1,
+                databaseReference
+        ) {
             @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-
-                if (firebaseAuth.getCurrentUser() != null) {
-
-                    startActivity(new Intent(MainActivity.this, AccountActivity.class));
-
-                }
-
+            protected void populateView(View v, String model, int position) {
+                TextView textView = (TextView) v.findViewById(android.R.id.text1);
+                textView.setText(model);
             }
         };
 
-        mLoginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                startSignIn();
-
-            }
-        });
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        mAuth.addAuthStateListener(mAuthListener);
-    }
-
-    private void startSignIn() {
-        String email = mEmailField.getText().toString();
-        String password = mPasswordField.getText().toString();
-
-        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
-            Toast.makeText(MainActivity.this, "Fields are empty.", Toast.LENGTH_LONG).show();
-        } else {
-
-            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-
-                    if (!task.isSuccessful()) {
-                        Toast.makeText(MainActivity.this, "Sign In Problem", Toast.LENGTH_LONG).show();
-                    }
-
-                }
-            });
-        }
+        mListView.setAdapter(firebaseListAdapter);
     }
 }
--- app/src/main/java/com/example/user/medconnect/Medconnect.java
+++ app/src/main/java/com/example/user/medconnect/Medconnect.java
@@ -3,6 +3,8 @@ package com.example.user.medconnect;
 import android.app.Application;
 
 import com.firebase.client.Firebase;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * Created by user on 30.09.2016.
@@ -13,7 +15,9 @@ public class Medconnect extends Application {
     public void onCreate() {
         super.onCreate();
 
-        Firebase.setAndroidContext(this);
-
+        /*Newer version*/
+        if (!FirebaseApp.getApps(this).isEmpty()) {
+            FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+        }
     }
 }
