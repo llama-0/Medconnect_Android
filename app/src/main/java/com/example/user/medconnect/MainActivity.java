2736ec3d3566272add8e004a864e8a4a1b7df406
llama-0 <AnRozan93@mail.ru>
2016-10-02 14:03:40 +0300

    Firebase Storage - Uploading Images from device gallery

--- app/src/main/java/com/example/user/medconnect/MainActivity.java
+++ app/src/main/java/com/example/user/medconnect/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.user.medconnect;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -21,41 +23,75 @@ import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
 import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ListView mListView;
+    private Button mSelectImage;
 
+    private StorageReference mStorage;
+
+    private static final int GALLERY_INTENT = 2;
+
+    private ProgressDialog mProgressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mListView = (ListView) findViewById(R.id.listView);
+        mStorage = FirebaseStorage.getInstance().getReference();
 
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://medconnect-1c3b5.firebaseio.com/Users");
+        mSelectImage = (Button) findViewById(R.id.selectImage);
 
-        FirebaseListAdapter<String> firebaseListAdapter = new FirebaseListAdapter<String>(
-                this,
-                String.class,
-                android.R.layout.simple_list_item_1,
-                databaseReference
-        ) {
+        mProgressDialog = new ProgressDialog(this);
+
+        //DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://medconnect-1c3b5.firebaseio.com/Users");
+
+        mSelectImage.setOnClickListener(new View.OnClickListener() {
             @Override
-            protected void populateView(View v, String model, int position) {
-                TextView textView = (TextView) v.findViewById(android.R.id.text1);
-                textView.setText(model);
-            }
-        };
+            public void onClick(View v) {
 
-        mListView.setAdapter(firebaseListAdapter);
+                Intent intent = new Intent(Intent.ACTION_PICK);
+
+                intent.setType("image/*");
+
+                startActivityForResult(intent, GALLERY_INTENT);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == GALLERY_INTENT && resultCode == RESULT_OK) {
+
+            mProgressDialog.setMessage("Uploading...");
+
+            Uri uri = data.getData();
+
+            StorageReference filepath = mStorage.child("Photos").child(uri.getLastPathSegment());
+
+            filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                    Toast.makeText(MainActivity.this, "Upload Done.", Toast.LENGTH_LONG).show();
+                    mProgressDialog.dismiss();
+                }
+            });
+        }
     }
 }
